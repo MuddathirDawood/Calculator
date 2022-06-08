e0bd487f5a5d2cf4afd58977ebb4b167901b5929
MuddathirDawood <dmuddathir@gmail.com>
2022-06-08 14:04:54 +0200

    delete and clear

--- script/code.js
+++ script/code.js
@@ -1,25 +1,19 @@
-let sum = '';
 function addNumb(numb){
     document.querySelector('#output').innerHTML += numb;
-    sum += numb;
 }
 
 function addOp(operator){
     document.querySelector('#output').innerHTML += operator;
-    sum += operator;
 }
 
 function addDec(decimal) {
     document.querySelector('#output').innerHTML += decimal;
-    sum += decimal;
 }
 
 function equal(){
-    sum = eval(sum)
-    document.querySelector('#output').innerHTML = sum;
+    document.querySelector('#output').innerHTML = eval(document.querySelector('#output').innerHTML);
 }
 
-function reset(clear){
-    sum = ' ';
-    document.querySelector('#output').value = sum;
-}
+// function reset(clear){
+//     document.querySelector('#output').value = ' ';
+// }
